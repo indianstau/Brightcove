3043fc4a540dcc1f8341fa42ec297e33131d2338
indianstau <indianstau@yahoo.com.tw>
2017-02-18 18:19:16 +0800

    Demo

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,8 @@ http://docs.brightcove.com/en/video-cloud/brightcove-player/guides/plugin-dev-qu
 
 第一步把plugin 掛上去
 主要是overlay
+
+有三種方法
+1.直接放文字上去
+2.放options 去代
+3.取變數overlayText 在json裡 可以方便客戶使用
--- plugin-dev.js
+++ plugin-dev.js
@@ -1,10 +1,12 @@
 /*pluginDev is function name, options is var from html, about 'this' is point to this function*/
 
-videojs.plugin('pluginDev', function () {
-    var player = this,var overlayText = "",
+videojs.plugin('pluginDev', function (options) {
+    var player = this,
+    /*var overlayText = "",*/
+        var options = {"overlayText":"this"}
         overlay = document.createElement('p')
     overlay.className = 'vjs-overlay'
-    overlay.innerHTML = overlayText
+    overlay.innerHTML = options.overlayText
     /*options.oeverlayText*/
     /*overlay.innerHTML = "Becoming a plugin developer";*/
     /*from html var options and text*/
