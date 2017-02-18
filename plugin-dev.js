184a6e40398c4b09533fed566d54b7dd9358a502
indianstau <indianstau@yahoo.com.tw>
2017-02-18 17:48:02 +0800

    Demo

--- plugin-dev.js
+++ plugin-dev.js
@@ -1,11 +1,12 @@
 /*pluginDev is function name, options is var from html, about 'this' is point to this function*/
-var overlayText = ""
+
 videojs.plugin('pluginDev', function () {
     var player = this,
         overlay = document.createElement('p')
     overlay.className = 'vjs-overlay'
-    overlay.innerHTML = overlayText
-    /*overlay.innerHTML = "Becoming a plugin developer";*/
+    /*overlay.innerHTML = overlayText*/
+    /*options.oeverlayText*/
+    overlay.innerHTML = "Becoming a plugin developer";
     /*from html var options and text*/
     player.el().appendChild(overlay)
     /*el() :Get the Components DOM element*/
