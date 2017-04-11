7627f7dd0f6cc83335e591fa22097913b2971f7f
indianstau <indianstau@yahoo.com.tw>
2017-04-11 14:36:38 +0800

    TCDP

--- Texttrack_CDsp_plugin.js
+++ Texttrack_CDsp_plugin.js
@@ -21,32 +21,25 @@ videojs.plugin('videolist', function () {
     //將data存在textTracks裡,從Json轉成JS物件存取出來
     var jsonData, sec, cn, fr, en;
     player.one("loadedmetadata", function () {
-        console.log('Here one ')
         var tt = player.textTracks()[1];
-        console.log(tt);
         tt.oncuechange = function (){
             if (tt.activeCues[0] !== undefined){
-                console.log(tt.activeCues[0]);
                 jsonData = JSON.parse(tt.activeCues[0].text);
                 sec = jsonData.sec;
                 en = jsonData.language.English;
                 fr = jsonData.language.France;
                 cn = jsonData.language.Chinese;
-                console.log(cn);
             }
         }
-    });
+    });    
     
-    var movieName = NewDiv2.childNodes;
-    
+    var movieName = NewDiv2.childNodes;    
     for (var i=0; i<6; i++) {   
     var NewBS1 = document.createElement('div');
         NewBS1.appendChild(document.createTextNode(fr[i]));
         NewDiv2.appendChild(NewBS1);          
         spacer.appendChild(NewDiv2);
         addMovieClickEvent(movieName[i], sec[i]);
-        console.log(movieName[3]);
-        console.log(sec[3]);
     }
     
     function addMovieClickEvent (node, secs) {
