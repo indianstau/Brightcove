f61dbe265800ae8fc2bf1de882771507afd7821d
indianstau <indianstau@yahoo.com.tw>
2017-04-11 11:54:24 +0800

    TCDP

--- Texttrack_CDsp_plugin.js
+++ Texttrack_CDsp_plugin.js
@@ -37,33 +37,15 @@ videojs.plugin('videolist', function () {
     
     for (var i=0; i<6; i++) {   
     var NewBS1 = document.createElement('div');
-        NewBS1.appendChild(document.createTextNode(en[i]));
+        NewBS1.appendChild(document.createTextNode(fr[i]));
         NewDiv2.appendChild(NewBS1);          
         spacer.appendChild(NewDiv2);
-        addMovieClickEvent(en[i], sec[i]);
+        addMovieClickEvent(fr[i], sec[i]);
     }
     
     function addMovieClickEvent (node, secs) {
         node.onclick = function() { player.currentTime(secs); }
     } 
-    
-    
-//    var movieN = ["Vampire Diaries","Lost Girl","Orange Is the New Black",
-//                  "Orphan Black","Rizzoli & Isles","Teen Wolf"];
-//    var movieSec = [0,126,284,401,515,650];
-//    var movieName = NewDiv2.childNodes;
-//    
-//    for (var i=0; i<6; i++) {   
-//    var NewBS1 = document.createElement('div');
-//        NewBS1.appendChild(document.createTextNode(movieN[i]));
-//        NewDiv2.appendChild(NewBS1);          
-//        spacer.appendChild(NewDiv2);
-//        addMovieClickEvent(movieName[i], movieSec[i]);
-//    }
-//    
-//    function addMovieClickEvent (node, secs) {
-//        node.onclick = function() { player.currentTime(secs); }
-//    }   
     
     //show dropdown        
     NewImg.onclick = function () {
