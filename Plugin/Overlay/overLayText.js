3b95d72157c2aef2a8b4842cc897b304af492588
indianstau <indianstau@yahoo.com.tw>
2017-07-19 19:45:31 +0800

    Update overLayText.js

--- Plugin/Overlay/overLayText.js
+++ Plugin/Overlay/overLayText.js
@@ -1,43 +1,12 @@
-/*plugin 方式*/
-
 videojs.plugin('plugintry',function (options) {
-    var myplayer = this;
-
-/*用於加在html上
-videojs('myPlayerID').ready(function() {
-    myplayer = this;
-                                                   */
-    
-   /* console.log('ready event trigger');   debug用*/
-            
-   /*https://docs.brightcove.com/en/perform/brightcove-player/guides/adding-captions-to-videos.html   字幕設定*/
-    
-    newSettings = {
-        'fontFamily': 'casual',
-        'edgeStyle': 'Dropshadow',
-        'backgroundOpacity': '0',
-    };
-    myplayer.textTrackSettings.setValues(newSettings);
-
-    myplayer.one('loadedmetadata',function() {
-       /* console.log('event trigger');
-        /* myplayer.muted(true);  靜音*/
-        
-        /*直接加字幕   https://docs.brightcove.com/en/perform/brightcove-player/guides/adding-captions-to-videos.html*/
-        var frTrack = myplayer.addRemoteTextTrack({
-            kind: 'captions',
-            language:'fr',
-            label: 'French',
-            src:'http://solutions.brightcove.com/bcls/captions/adding_captions_to_videos_french.vtt'
-        });
-        
+    var myplayer = this;         
         myplayer.overlay({
             overlays:
             [
                 {   align:'top-right',
                     content:'<div id="one">'+
                             '<img id="close1">'+
-                            '<input id="ad" title="想吃漢堡嗎?">'+
+                            '<input id="ad" title="海尼根">'+
                             '</div>',
                     start:1,
                     end:10
@@ -45,32 +14,22 @@ videojs('myPlayerID').ready(function() {
                     align:'bottom-left',
                     content:'<div id="two">'+
                             '<img id="close2">'+
-                            '<input id="ad2" title="Bmx是一種極限運動">'+
+                            '<input id="ad2" title="參加抽獎">'+
                             '<a id="linkToweb"></a>'+
                             '</div>',
                     start:2,
                     end:15
-                },{
-                    align:'top-right',
-                    content:'<div id="time" title="我是時間"></div>',
-                    start:16,
-                    end:256
                 }
-            ]//lays
-        });//myplayer.overlay
-             /*-------JS createElement 'img' 給ID style 傳不進去 無效-----------------------*/
+            ]
+        });
         var divOne = document.getElementById('one'); 
         var nodeID = document.getElementById('close1');        
-           nodeID.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-close-circled-128.png";
-           nodeID.style = "height:20px;width:20px;position:absolute;left:17px;cursor:pointer;"  
-            
+           nodeID.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/High-contrast-dialog-close.svg/768px-High-contrast-dialog-close.svg.png";
+           nodeID.style = "height:20px;width:20px;position:absolute;left:17px;cursor:pointer;"             
         var node2 = document.getElementById('close2');                    
             node2.src= nodeID.src;
-            node2.style = "height:20px;width:20px;position:absolute;left:7px;top:7px;cursor:pointer";
-         
-        
-        var divTwo = document.getElementById('two');
-        
+            node2.style = "height:20px;width:20px;position:absolute;left:7px;top:7px;cursor:pointer";      
+        var divTwo = document.getElementById('two');      
         nodeID.onclick = function deleteP() {
            divOne.addEventListener('click',function(){
                divOne.style.display = 'none';  
@@ -81,38 +40,25 @@ videojs('myPlayerID').ready(function() {
                 divTwo.style.display = 'none';
            });
         };
-
         var ad1 = document.getElementById('ad');
             ad1.setAttribute('type','image');
-            ad1.src = 'https://lh5.googleusercontent.com/-0opy1PkE0a4/Vo6FyqGw1RI/AAAAAAAAByg/WMcbEd_nh-Q/tumblr_lrzjg5o0O61r3dgmdo1_r1_500.gif';
+            ad1.src = 'http://www.ezpr.com.tw/wp-content/uploads/2015/04/heineken-logo.png';
             ad1.style = "height:75px;width:100px;";             
         ad1.onclick = function link() {
-            window.open("https://www.bk.com/","_blank");
+            window.open("https://www.heineken.com/tw/agegateway?returnurl=%2ftw","_blank");
         };
-
         var ad2 = document.getElementById('ad2');
             ad2.setAttribute('type','image');
-            ad2.src = "http://mellonbmx.com/wp-content/uploads/2012/04/Temperedbikes_Anthony_ad_v21-e1335359573987.jpg";
-            ad2.style = "width:220px;height:70px;";        
+            ad2.src = "http://www.biosmonthly.com/uploads/content/%E3%80%90%E5%9C%96%E8%AA%AA%E4%B8%80%E3%80%91%E6%89%93%E9%96%8B%E6%B5%B7%E5%B0%BC%E6%A0%B9%EF%BC%8C%E4%B8%80%E5%90%8C%E5%8F%83%E5%8A%A0%E4%B8%96%E7%95%8C%E5%9F%8E%E5%B8%82%E5%A4%A7%E6%8A%BD%E7%8D%8E%EF%BC%81.jpg";
+            ad2.style = "width:160px;height:120px;";    
         ad2.onclick = function openYu(){
-            window.open("https://www.youtube.com/watch?v=Y31E2-u6Arw","_blank");
+            window.open("https://www.youtube.com/watch?v=VBljxzguKVw","_blank");
         };     
         var linkTo = document.getElementById('linkToweb');
-            linkTo.href = "http://bmx.transworld.net/";
+            linkTo.href = "https://www.heineken.com/tw/agegateway?returnurl=%2ftw";
             linkTo.target = "_blank";
             linkTo.style = "padding-left:10px;";
-            linkTo.innerHTML = "BMX: STREET FINALS - SIMPLE SESSION 2017";
-        
-        /*時間*/
-        var myVar = setInterval(myTimer , 1000);
-        function myTimer () {
-            var d = new Date();
-            document.getElementById('time').innerHTML = d.toLocaleTimeString();
-        };
-        
-    });//on
-
-/* });//ready */
-    
-});//plugin
+            linkTo.innerHTML = "我以為海尼根不會再更過火，但是我錯了";      
+    })   
+});
 
