8d421207510b4e8f5fdbec5f0208b75db410941e
indianstau <indianstau@yahoo.com.tw>
2017-09-11 14:17:28 +0800

    thu

--- Plugin/thumbnails/thumbnails.js
+++ Plugin/thumbnails/thumbnails.js
@@ -25,7 +25,9 @@ videojs.plugin('thumbs', function(){
         secGroup = [];                                                
         for(var i=0; i<data.length; i++){   
             secGroup[secAry] = ({src: url + count + jpg});
-            secGroup[0].style = ({left: '-40px',width: '80px',height: '60px'});             
+            secGroup[0].style = ({left: '0',width: '80px',height: '60px'});  
+            if(i>11) { secGroup[secAry].style = ({left: '-40px'}); }
+            if(i>182) { secGroup[secAry].style = ({left: '-80px'}); }
             count += 1;             
             secAry += data.sec;                               
         }
