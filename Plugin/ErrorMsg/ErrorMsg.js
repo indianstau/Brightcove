4ae5cbc6f309fb5867795e87e2243c176d1e3fe8
indianstau <indianstau@yahoo.com.tw>
2017-12-05 11:39:17 +0800

    errorMsg

--- Plugin/ErrorMsg/ErrorMsg.js
+++ Plugin/ErrorMsg/ErrorMsg.js
@@ -1,14 +1,45 @@
-videojs.plugin("ErrorMsg",function(){
-var spacer = document.getElementsByClassName("vjs-spacer")[0]; 
-var newP = document.createElement('p');
-    newP.id ="eMark";
-    newP.textContent = "!";
-    newP.title = "Problem report";
-    newP.setAttribute("style","font-family:serif;font-size:18px;font-weight:bold;color:#fff;cursor:pointer;margin-top:6px;");
-    spacer.setAttribute("style","justify-content:flex-end;");
-    spacer.appendChild(newP); 
-    newP.onclick = function(){
-        window.open('https://docs.google.com/forms/d/1L4dnmWEyZ0F9Ji_KETUaewwCEFw3-uHBnoOHydb-sOU/edit?usp=sharing','Brightcove問題回報');
-    };
+videojs.registerPlugin("ErrorMsg",function(){
     
+var player = this;
+    
+    player.one("loadstart",function(){
+        var spacer = document.getElementsByClassName("vjs-spacer")[0]; 
+        var newElbtn = document.createElement('button');
+        newElbtn.id = "eMarkBtn";
+        newElbtn.style.width = "30px";
+        newElbtn.style.height = "20px";
+        newElbtn.style.marginTop = "6px";
+        newElbtn.style.display = "block";
+        newElbtn.style.paddingRight = "12px";
+
+        var newEli = document.createElement('i');
+        newEli.id ="eMarki";
+        newEli.title = "Problem report";
+        newEli.setAttribute("style",";cursor:pointer;font-size: 15px;padding-left: 8px;");
+
+        var newEla = document.createElement('a');
+        newEla.id = "itema";
+        newEla.textContent = "問題回報";
+        newEla.className = "errorReporta";
+        newEla.setAttribute("href","https://docs.google.com/forms/d/1L4dnmWEyZ0F9Ji_KETUaewwCEFw3-uHBnoOHydb-sOU/edit?usp=sharing");
+        newEla.setAttribute("target","_blank");
+
+        spacer.setAttribute("style","justify-content:flex-end;");
+        newElbtn.appendChild(newEla);
+        newElbtn.appendChild(newEli); 
+        spacer.appendChild(newElbtn);    
+
+        var btn = document.getElementById("eMarkBtn");
+        btn.onclick = function(){
+            newEla.classList.toggle("show");
+        };
+
+        window.onclick = function(event){
+            if(!event.target.matches('#eMarki')){
+                if(newEla.classList.contains('show')){
+                    newEla.classList.remove('show');
+                }
+            }
+        }; 
+    });   
 });
