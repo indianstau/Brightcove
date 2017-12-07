6839feba347747ed11d1f5747011ec20a1e16e8a
indianstau <indianstau@yahoo.com.tw>
2017-12-07 16:13:53 +0800

    ErrorOverlay

---
+++ Plugin/ErrorMsg/ErrorOverlay.js
@@ -0,0 +1,15 @@
+videojs.registerPlugin("ErrorMsgOverlay",function(){
+    var player = videojs("vjs_video_3");
+    player.one("loadedmetadata",function(){
+        player.on("error",function(){
+        var errorOKbtn = document.getElementsByClassName("vjs-errors-ok-button-container")[0];
+        var newElbtn = document.createElement('button');
+            newElbtn.id = "eMarki";
+            newElbtn.textContent = "Report";
+            newElbtn.onclick = function(){
+                window.open("https://docs.google.com/forms/d/1L4dnmWEyZ0F9Ji_KETUaewwCEFw3-uHBnoOHydb-sOU/edit?usp=sharing");
+            };
+            errorOKbtn.appendChild(newElbtn);   
+        }); 
+    });    
+});
