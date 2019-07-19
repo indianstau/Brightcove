068ffd21a94a728f39e29ee8e2585fc0a47b04f1
indianstau <indianstau@yahoo.com.tw>
2019-07-19 15:37:11 +0800

    Update overMsg.js

--- Gallery/Live event demo/overMsg.js
+++ Gallery/Live event demo/overMsg.js
@@ -1,11 +1,13 @@
 var player, box, text;
 videojs.registerPlugin('overMsg',function(){
-    console.log('Here is plugin inside');
-    player = document.getElementById('performPlayer_html5_api');
-    box = document.getElementsByClassName('vjs-overlay vjs-overlay-top vjs-overlay-background')[0]
-    text = document.getElementsByClassName('text')[0];
+    videojs.getPlayer('performPlayer_html5_api').ready(function(){
+        player = this;
+        console.log('Here is plugin inside');
+        box = document.getElementsByClassName('vjs-overlay vjs-overlay-top vjs-overlay-background')[0]
+        text = document.getElementsByClassName('text')[0];
         player.on('ended',function(){
             setTimeout('box.style.display = "none";',5000);
             console.log('Here is ended event!');
         }) 
+    });
 });
