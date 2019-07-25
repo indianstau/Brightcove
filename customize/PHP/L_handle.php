3aa40a9bf302d11dfd48f78a5ab429094fb34fdf
indianstau <indianstau@yahoo.com.tw>
2019-07-25 15:12:54 +0800

    Create L_handle.php

---
+++ customize/PHP/L_handle.php
@@ -0,0 +1,30 @@
+<?php
+// POST won't work for JSON data
+$problem = "No errors";
+try {
+    $json    = file_get_contents('php://input');
+    $decoded = json_decode($json, true);
+} catch (Exception $e) {
+    $problem = $e->getMessage();
+}
+
+date_default_timezone_set("Asia/Taipei");
+$decoded['time'] = date("Y-m-d H:i:s");
+
+// turn notification into pretty printed JSON
+$notification = json_encode($decoded, JSON_PRETTY_PRINT);
+
+$logEntry = $notification.",";
+//"\nErrors receiving notificatons: ".$problem.
+//"\n--------------".date("Y-m-d H:i:s")."-----------------\n";
+
+// Lastly, tell PHP where it can find the log file and tell PHP to open it
+// and add the string we created earlier to it.
+$logFileLocation = "di-log.txt";
+$fileHandle      = fopen($logFileLocation, 'a') or die("-1");
+fwrite($fileHandle, $logEntry);
+fclose($fileHandle);
+
+// line below is displayed when you browse the app directly
+echo "Dynamic Ingest callback app is running";
+?>
